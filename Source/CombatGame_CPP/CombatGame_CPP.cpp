691b65d909bb7c767b6f572639c1736d67fd9e3b
gwl687 <475857172@qq.com>
2025-07-13 14:15:07 +0900

    v1

--- Source/CombatGame_CPP/CombatGame_CPP.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CombatGame_CPP.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CombatGame_CPP, "CombatGame_CPP" );
